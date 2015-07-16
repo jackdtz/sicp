2859017fd7316a2eba856ab8d7a265082db03dd6
Jack <dongtianzheng@gmail.com>
2015-07-15 23:46:30 -0400

    update for ex 2.1

--- 2-1/2-1.scm
+++ 2-1/2-1.scm
@@ -1,5 +1,9 @@
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ; Exercise 2.1
+
+; solution 1
 
 (define (make-rat n d)
   
@@ -15,3 +19,21 @@
   (cond ((and (< n 0) (< d 0)) (make-rat-reduce (- n) (- d)))
         ((and (> n 0) (< d 0)) (make-rat-reduce (- n) (- d)))
         (else (make-rat-reduce n d))))
+
+
+; solution 2
+(define (make-rat n d)
+  (define (gcd a b)
+    (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+  
+  (define (make-rat-reduce n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  
+  (if (< d 0)
+      (make-rat-reduce (- n) (- d))
+      (make-rat-reduce n d)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
